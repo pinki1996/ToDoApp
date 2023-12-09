bb991559826f762f0f5d8e5fd372f002506248c6
pinki1996 <120261749+pinki1996@users.noreply.github.com>
2023-12-09 18:23:05 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,80 +1,26 @@
-//useState Hooks: it is used to manage your state(data) in functiona component
-//import useState from 'react'
-//decalre hooks at the top level
-//useState = variable, updating function
-//const count = useState(initalvalue)
 
-
-
+import TaskForm from './Components/TaskForm.js';
 import './App.css';
-import { useState, useRef } from 'react';
+import TaskList from './Components/TaskList.js';
+import { useState } from 'react';
 
-// state:data
-//uncontrolled {useRef} vs controlled {useState}
 
 function App() {
-    const data = localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : [];
-    const [list, setList] = useState(data);
-    const [newTask,setNewTask] = useState('') 
-    const [search,setSearch] = useState('');
-    console.log(list)
 
-    const addTask = () => {
-        localStorage.setItem('list',JSON.stringify([...list, newTask]))
-        setList([...list, newTask ])
-        setNewTask("")
-        
-    }
-    const deleteTask = (i) => {
-        const delList = [...list];
-        delList.splice(i, 1);
-        setList(delList)
-        localStorage.setItem('list',JSON.stringify(delList))
-    }
-    const updateTask = (e, i) => {
-        const upTask = [...list];
-        upTask.splice(i, 1, e.target.value)
-        setList(upTask)
-        localStorage.setItem('list',JSON.stringify(upTask))
-    }
-   
-    const keyEnter =(e)=>{
-        if(e.key ==="Enter"){
-            addTask()
-        } 
-    }
+  const[tasks, setTasks] = useState([])
 
-
-    return (
-        <center>
-            
-
-                <div className="search">
-                    <input type="text" placeholder='search task search 🏸' onChange={(e)=>{setSearch(e.target.value)}}/>
-                </div>
-            <h1 className='heading'>To-Do App ✨</h1>
-            <div className="inputs">
-
-                <input type="text" onChange={(e)=>{setNewTask(e.target.value)}} onKeyDown={keyEnter} value={newTask} />
-                <button className="btn" onClick={addTask}>Ask task ✌</button>
-            </div>
-            <div className="container">
-
-                {
-                    list.map((val, i) => {
-                        if(val.toLowerCase().includes(search.toLowerCase())){
-                        return (
-                            <div className='list' key={i}>
-                                <input className='text' value={val} onChange={(e) => updateTask(e, i)} />
-                                <span className="icon" onClick={() => { deleteTask(i) }}>❌</span>
-                            </div>
-                        )
-                        }
-                    })
-                }
-            </div>
-        </center>
-    );
+  return (
+    <div className="bg-slate-300 min-h-screen pt-4">
+    <div className="max-w-xl mx-auto shadow-xl p-4 bg-white rounded-lg">
+      <h3 className="text-center mt-4">Task Management Application</h3>
+      <TaskForm tasks={tasks} setTasks={setTasks} />
+    </div>
+    <div className="max-w-xl mx-auto shadow-xl p-4 bg-white rounded-lg mt-8">
+      <TaskList tasks={tasks} setTasks={setTasks} />
+    </div>
+    ̦
+  </div>
+  );
 }
 
 export default App;
